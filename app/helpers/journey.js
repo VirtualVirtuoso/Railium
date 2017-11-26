36a7bd686a1e5474dfc39903731364d2e8cf6256
webbo89 <webbo89@gmail.com>
2017-11-26 01:12:56 +0100

    Adding helper file

---
+++ app/helpers/journey.js
@@ -0,0 +1,45 @@
+
+
+
+function JourneyStats(journey) {
+
+    this.journey = journey;
+    this.stats = function() {
+
+        this.response = {
+
+        }
+        for(var i = 0; i < this.journey['crowd_answers'].length;i++){
+            question = this.journey['crowd_answers'][i];
+
+            qtype = question['qtype'];
+
+
+            if ( typeof this.response[qtype + 'count'] !== 'undefined')
+            {
+                this.response[qtype + 'count']++;
+            } else {
+                this.response[qtype + 'count'] = 1;
+            }
+
+            switch(question['qtype']){
+                case 'toilets':
+                    console.log('evaluateloo');
+                    break;
+
+                case 'toohot':
+                    console.log('toohot');
+                    break;
+
+            }
+
+        }
+
+        return this.response
+            console.log(this.response)
+            console.log('hello')
+    }
+};
+
+
+module.exports = JourneyStats;
