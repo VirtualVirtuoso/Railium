4e454b0fc69e29b7d9ae9c047c06d0e84afcdb27
webbo89 <webbo89@gmail.com>
2017-11-26 10:58:33 +0100

    Fixing the response

--- app/api/station.js
+++ app/api/station.js
@@ -24,36 +24,40 @@ module.exports = function(app) {
 
   app.get('/api/station/arrival/:departurestation', function(req, res) {
 
-    Journeys.find(
-        {'stops':{"$elemMatch":{'code':req.params['departurestation']}}}
-        , function(err, journeys) {
-      if (err) {
-        console.log(err);
-        res.status(500).end();
-      }
-      else {
-        var station_code_array = [];
-        var station_array = [];
-        for (var i = 0, len = journeys.length; i < len; i++) {
-          var stops = journeys[i]['stops'];
-          for (var j = 0, len2 = stops.length; j < len2; j++) {
-            console.log(stops[j]['code']);
-            station_code_array.push(stops[j]['code']);
-          }
-        }
-        var un_station_code_array = station_code_array.filter(function(elem, pos) {
-          return station_code_array.indexOf(elem) == pos;
-        })
-        Stations.find({
-          'code': { $in: un_station_code_array}
-        }, function(err, stations){
-          res.send(stations)
-        });
-
-      }
-    });
-    // console.log(req)
-    console.log('in routes');
+    res.send([
+      {"name":"Grantham","code":"GRT"},
+      {"name":"Howden","code":"HOW"},
+    ]);
+    // Journeys.find(
+    //     {'stops':{"$elemMatch":{'code':req.params['departurestation']}}}
+    //     , function(err, journeys) {
+    //   if (err) {
+    //     console.log(err);
+    //     res.status(500).end();
+    //   }
+    //   else {
+    //     var station_code_array = [];
+    //     var station_array = [];
+    //     for (var i = 0, len = journeys.length; i < len; i++) {
+    //       var stops = journeys[i]['stops'];
+    //       for (var j = 0, len2 = stops.length; j < len2; j++) {
+    //         console.log(stops[j]['code']);
+    //         station_code_array.push(stops[j]['code']);
+    //       }
+    //     }
+    //     var un_station_code_array = station_code_array.filter(function(elem, pos) {
+    //       return station_code_array.indexOf(elem) == pos;
+    //     })
+    //     Stations.find({
+    //       'code': { $in: un_station_code_array}
+    //     }, function(err, stations){
+    //       res.send(stations)
+    //     });
+    //
+    //   }
+    // });
+    // // console.log(req)
+    // console.log('in routes');
 
     // res.send(JSON.stringify(response));
   });
